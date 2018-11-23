94b8325c4fd28e803b88bb9438ac827bdf80327d
Subhash Vadadoriya <it.subhashpatel@gmail.com>
2018-11-23 10:39:00 +0530

    add script run as a root

--- install-LAMP-ubuntu.sh
+++ install-LAMP-ubuntu.sh
@@ -4,6 +4,12 @@
 # Bash script to install an LAMP stack in ubuntu
 # Author: Subhash (serverkaka.com)
 
+# Check if running as root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 # Update system
 sudo apt-get update -y
 
